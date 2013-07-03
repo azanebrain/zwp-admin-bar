cc8e4d864c58d2a84de774faf653f9376da43b22
AJZane <androiddreams@AJZane.com>
2013-07-03 10:47:44 -0700

    attempting to figure out if the target CSS file exists am currently unable to get a boolean indication

--- init.php
+++ init.php
@@ -16,12 +16,58 @@ function wp_set_admin_bar_color($path_to_z_wp_admin_bar_dir = "functions/") {
 
 	}
 	
+	$path_to_z_wp_admin_bar_dir = "/cms/wp-content/themes/watson2012/functions/";
+	// $zwp_adminbar_style = $GLOBALS['template_directory'] . "/" . $path_to_z_wp_admin_bar_dir . 'z-wp-admin-bar/styles/'.WP_ENV.'.css';
 	$zwp_adminbar_style_file = $path_to_z_wp_admin_bar_dir . 'z-wp-admin-bar/styles/'.WP_ENV.'.css';
 
-	//Now enqueue the stylesheet
-	wp_enqueue_style('admin_bar', $zwp_adminbar_style_file, array(), false, 'all');
+	echo "stylesheet dir: ". get_bloginfo('stylesheet_url')."!<br>\n";
+	echo "dirnam of that: " .dirname( get_bloginfo('stylesheet_url') )."!<br>\n";
+	echo "adminbar string: <b>".$zwp_adminbar_style."</b>!<br>\n";
+	echo "is file : ".   is_file( $zwp_adminbar_style_file ) . " ! <BR>\n";
+	echo "file_exists : ".   file_exists( $zwp_adminbar_style ) . " ! <BR>\n";
+	// echo "fopen : ".   fopen( $zwp_adminbar_style , 'r' ) . " ! <BR>\n";
+
+	//Now enqueue the stylesheet if it exists:
+	if( is_file( $zwp_adminbar_style_file ) ) {
+		// add_action( 'wp_enqueue_scripts', 'wp_set_admin_bar_color' );
+		wp_enqueue_style('admin_bar', $zwp_adminbar_style_file, array(), false, 'all'); 
+	} else if ( 
+		! has_filter("z_debug") && 'prod' != WP_ENV
+		|| has_filter("z_debug") && "off" != apply_filters("z_debug", "")
+		){
+		// has_filter("z_debug") && "on" == apply_filters("z_debug", "")
+		// || WP_ENV != 'prod' && "off" != apply_filters("z_debug", "")
+
+//If the debug has not been set, and the environment is not production: Display the debug message, unless on production (default)
+//If the debug has been set, and is not set to off
+
+		//The CSS file cannot be found
+		//display an error message
+		echo "\n<strong>ERROR</strong> in Z WP Admin Bar: Path to style file (" . $zwp_adminbar_style_file . ") cannot be found.\n";
+	}//if
+
 
 }//wp_set_admin_bar_color
+
+
+/* TEST AREA
+ * Testing the hooks
+ */
+
+function modify_z_wp_admin_bar_dir(){
+	//Note that the path ends in a trailing slash
+	return "functions/";
+}
+//add_filter("z_wp_admin_bar_dir", "modify_z_wp_admin_bar_dir");
+
+function z_debug(){
+	// return "on";
+	//return "off";
+	// return "ha!";
+}
+
+add_filter("z_debug", "z_debug");
+
 
 // add_action( 'wp_before_admin_bar_render', 'wp_set_admin_bar_color' );
 wp_set_admin_bar_color();
