839af695079c035b2d4b24a002d1ced40bf679cd
AJ Zane <androiddreams@ajzane.com>
2013-12-14 18:39:11 -0800

    renamed the main file, added functionality to hide the plugin from the list once it is activated

--- zwp-admin-bar.php
+++ zwp-admin-bar.php
@@ -82,3 +82,29 @@ function wp_set_admin_bar_color($path_to_z_wp_admin_bar_dir = "functions/") {
 
 // add_action( 'wp_before_admin_bar_render', 'wp_set_admin_bar_color' );
 wp_set_admin_bar_color();
+
+
+//Hide this plugin from the plugin list once it is activated
+add_filter( 'all_plugins', 'hide_zwp_admin_bar_from_plugin_list' );
+
+function hide_zwp_admin_bar_from_plugin_list( $plugins ) {
+    unset( $plugins[ 'zwp-admin-bar/zwp-admin-bar.php' ] );
+    return $plugins;
+}
+
+/*  Copyright 2013 AJ Zane  (email : androiddreams@AJZane.com)
+
+	This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
